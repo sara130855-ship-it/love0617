c21217ebabb9d5b57c571a54c7cfe8b34c349203
LOVE0617 <sara130855@gmail.com>
2025-11-12 16:45:40 +0800

    Update main.js

--- Web/021/src/main.js
+++ Web/021/src/main.js
@@ -18,7 +18,7 @@ render(App);
 
 // Webpack Hot Module Replacement API
 if (module.hot) {
-    module.hot.accept('./app', () => {
+    module.hot.accept('./app.js', () => {
         render(App);
     })
 }
